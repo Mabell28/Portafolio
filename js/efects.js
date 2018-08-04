2758ecae4a5b1911b6a4b1775b7dfdb17302989d
Mabell28 <mabellhuayanaytaype@gmail.com>
2018-08-03 23:38:31 -0500

    cambio texto- foto

---
+++ js/efects.js
@@ -0,0 +1,38 @@
+
+	
+  $(window).load(function(){
+    
+
+	"use strict";
+
+  var firstText = $("#t-1");
+  var secondText = $('#t-2');
+
+  firstText.slideDown(800);
+  secondText.delay(1000);
+  secondText.slideDown(800);
+
+  
+
+  
+  
+
+
+
+});
+
+
+ 
+
+      
+
+  	
+
+  
+
+
+  
+ 
+
+
+
